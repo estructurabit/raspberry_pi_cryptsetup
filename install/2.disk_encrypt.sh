5776ca316a5dd9bfe87c8866f36949b9df38fef2
gusluke <gusluke@gmail.com>
2024-11-15 11:43:41 +0100

    first commit

---
+++ install/2.disk_encrypt.sh
@@ -0,0 +1,33 @@
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+apt-get install busybox cryptsetup cryptsetup-initramfs initramfs-tools -y
+apt-get install expect --no-install-recommends -y
+cp /boot/install/initramfs-rebuild /etc/kernel/postinst.d/initramfs-rebuild
+cp /boot/install/resize2fs /etc/initramfs-tools/hooks/resize2fs
+chmod +x /etc/kernel/postinst.d/initramfs-rebuild
+chmod +x /etc/initramfs-tools/hooks/resize2fs
+
+echo 'CRYPTSETUP=y' | tee --append /etc/cryptsetup-initramfs/conf-hook > /dev/null
+mkinitramfs -o /boot/firmware/initramfs.gz
+
+lsinitramfs /boot/firmware/initramfs.gz | grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
+#Make sure you see sbin/resize2fs, sbin/cryptsetup, and sbin/fdisk in the output.
+
+echo 'initramfs initramfs.gz followkernel' | tee --append /boot/firmware/config.txt > /dev/null
+
+sed -i '$s/$/ cryptdevice=\/dev\/mmcblk0p2:sdcard/' /boot/firmware/cmdline.txt
+
+ROOT_CMD="$(sed -n 's|^.*root=\(\S\+\)\s.*|\1|p' /boot/firmware/cmdline.txt)"
+sed -i -e "s|$ROOT_CMD|/dev/mapper/sdcard|g" /boot/firmware/cmdline.txt
+
+FSTAB_CMD="$(blkid | sed -n '/dev\/mmcblk0p2/s/.*\ PARTUUID=\"\([^\"]*\)\".*/\1/p')"
+sed -i -e "s|PARTUUID=$FSTAB_CMD|/dev/mapper/sdcard|g" /etc/fstab
+
+echo 'sdcard /dev/mmcblk0p2 none luks' | tee --append /etc/crypttab > /dev/null
+
+echo "Done. Reboot with: sudo reboot"
+#reboot
---
+++ install/3.disk_encrypt_initramfs.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+e2fsck -f /dev/mmcblk0p2
+resize2fs -fM /dev/mmcblk0p2
+
+BLOCK_COUNT="$(dumpe2fs /dev/mmcblk0p2 | sed "s/ //g" | sed -n "/Blockcount:/p" | cut -d ":" -f 2)"
+echo "Block count of SD partition: $BLOCK_COUNT"
+
+SHA1SUM_ROOT="$(dd bs=4k count=$BLOCK_COUNT if=/dev/mmcblk0p2 | sha1sum)"
+echo "SHA1SUM_ROOT of SD partition: $SHA1SUM_ROOT"
+
+echo "Now we will clone SD card to USB, please wait..."
+dd bs=4k count=$BLOCK_COUNT if=/dev/mmcblk0p2 of=/dev/$1
+echo "Clone from SD to USB finished..."
+
+SHA1SUM_EXT="$(dd bs=4k count=$BLOCK_COUNT if=/dev/$1 | sha1sum)"
+echo "SHA1SUM_EXT of USB partition: $SHA1SUM_EXT"
+
+if [ "$SHA1SUM_ROOT" == "$SHA1SUM_EXT" ]; then
+	echo "1.Sha1sums match."
+        echo "Now we will encrypt sd card partition"
+	cryptsetup --cipher aes-cbc-essiv:sha256 luksFormat /dev/mmcblk0p2
+	cryptsetup luksOpen /dev/mmcblk0p2 sdcard
+	
+	echo "Encryption completed, now we will restore USB partition onto SD card, please wait..."
+	dd bs=4k count=$BLOCK_COUNT if=/dev/$1 of=/dev/mapper/sdcard
+	
+	echo "Restore completed, now we will validate the restore with the SHA1SUM_NEWROOT, please wait..."
+	SHA1SUM_NEWROOT="$(dd bs=4k count=1516179 if=/dev/mapper/sdcard | sha1sum)"
+	
+	echo "Restore completed"
+	
+	if [ "$SHA1SUM_ROOT" == "$SHA1SUM_EXT" ]; then
+		echo "2.Sha1sums match."
+		e2fsck -f /dev/mapper/sdcard
+		resize2fs -f /dev/mapper/sdcard
+		cryptsetup luksClose /dev/mapper/sdcard
+		echo "Done. Reboot and rebuild initramfs."
+	else
+		echo "2.Sha1sums error."
+	fi
+else
+	echo "1.Sha1sums error."
+fi
